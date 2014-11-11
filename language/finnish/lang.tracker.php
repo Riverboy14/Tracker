6858cdd548dfacbc1e888a134d6217e0603b1223
Pete <yhteydenotot@luontotupa.net>
2014-11-11 22:53:51 +0200

    Finnish language for Tracker
    100% translated and ready

---
+++ language/finnish/lang.tracker.php
@@ -0,0 +1,578 @@
+<?php
+
+ /**
+ * Solspace - Tracker
+ *
+ * @package		Solspace:Tracker
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Tracker/
+ * @version		2.1.0
+ * @filesource 	./system/expressionengine/third_party/tracker/language/english/
+ */
+
+$lang = $L = array(
+
+//----------------------------------------
+//	Required for MODULES page
+//----------------------------------------
+
+"tracker_module_name" =>
+"Osumalaskuri",
+
+"tracker_label" =>
+"Osumalaskuri",
+
+"tracker_module_description" =>
+"Laskee julkaisujen katselukerrat ja näyttää yhteenvedon katselukerroista.",
+
+"tracker_module_version" =>
+"Osumalaskuri",
+
+
+//----------------------------------------
+//	Language for Menu
+//----------------------------------------
+
+"tracker" =>
+"Osumalaskuri",
+
+"online_documentation"	 =>
+(rand() < 42) ? "Ei hätää!" : "Käyttöohjeet verkossa",
+
+'tracker_utility' =>
+"Työkalu",
+
+'tracker_utilities' =>
+"Työkalut",
+
+'tracker_update_statistics' =>
+"Päivitä statistiikka",
+
+'unsupported_mysql_version' =>
+"Käytössäsi olevaa MySQL ei tueta. &nbsp; Ole hyvä ja ota yhteyttä palveluntarjoajaasi jotta voit päivittää MySQL version vastaamaan tämäm moduulin vaatimuksia.",
+
+//----------------------------------------
+//	Chart Tag
+//----------------------------------------
+
+'tracker_week' =>
+"Viikko",
+
+//----------------------------------------
+//	CP Main Menus
+//----------------------------------------
+
+"homepage" =>
+"Kotisivu",
+
+'tracker_statistics' =>
+"Statistiikat",
+
+'tracker_preferences' =>
+"Asetukset",
+
+// --------------------------------------------
+//  Preferences Area - Sections
+// --------------------------------------------
+
+'tracker_tracking' =>
+"Tiedonkeruu",
+
+'tracker_display_preferences' =>
+"Näytä",
+
+'tracker_member_group_permissions' =>
+"Jäsenryhmien oikeudet",
+
+// --------------------------------------------
+//  Preferences Area - Preferences
+// --------------------------------------------
+
+'track_views_member_groups' =>
+"Tallenna katselukerrat näiltä jäsenryhmiltä",
+
+'display_tracker_fieldtype_for_channels' =>
+"Näytä katselukertojen lomakekenttä kanaville",
+
+'display_tracker_stats_in_edit' =>
+"Näytä katselukertojen statistiikka muokkaustilassa",
+
+'display_tracker_stats_in_edit_time_range' =>
+"Näytä katselukertojen statistiikka muokkaustilassa: aikaväli",
+
+'display_tracker_stats_in_edit_label' =>
+"Näytä katselukertojen statistiikka muokkaustilassa: Sarakkeen otsake",
+
+'display_live_views_tracker_stats' =>
+"Näytä Juuri Nyt katseltavat julkaisut Osumalaskurin hallintapaneelissa",
+
+'display_live_views_tracker_stats_subtext' =>
+"Päivittää automaattisesti statistiikan valituille kanaville muutaman sekunnin välein.",
+
+'display_tracker_stats_column_in_edit' =>
+"Näyttää statistiikasarakkeen muokkauksessa",
+
+'display_tracker_stats_fieldtype_in_publish' =>
+"Näyttää statistiikkakentän julkaisutilassa",
+
+'display_statistics_in_cp' =>
+"Näyttää statistiikkavälilehden(tab) moduulin hallintapaneelissa",
+
+'display_charts_in_cp' =>
+"Näyttää kaaviot moduulin hallintapaneelissa",
+
+'display_utilities_in_cp' =>
+"Näyttää Työkalut-välilehden moduulin hallintapaneelissa",
+
+'display_preferences_in_cp' =>
+"Näyttää Asetukset-välilehden moduulin hallintapaneelissa",
+
+'display_documentation_in_cp' =>
+"Näyttää Käyttöohjeet-välilehden moduulin hallintapaneelissa",
+
+'can_update_module' =>
+"Voiko päivittää moduulin?",
+
+'tracker_stats' =>
+"Osumalaskurin statistiikat",
+
+'all_member_groups' =>
+"Kaikki jäsenryhmät",
+
+'all_channels' =>
+"Kaikki kanavat",
+
+'today' =>
+"Tänään",
+
+'yesterday' =>
+"Eilen",
+
+'current_week' =>
+"Nykyinen viikko",
+
+'current_month' =>
+"Nykyinen kuukausi",
+
+'current_year' =>
+"Nykyinen vuosi",
+
+'save_preferences' =>
+"Tallenna tämä asetus",
+
+'tracker_preferences_updated' =>
+"Asetukset päivitetty!",
+
+'stats_update_interval' =>
+"Statistiikan päivitysväli",
+
+'stats_update_interval_subtext' =>
+"Määritelty aikaväli katselukertojen laskentaan julkaisun statistikkan suhteen.",
+
+'5_minutes' =>
+"5 minuuttia",
+
+'10_minutes' =>
+"10 minuuttia",
+
+'30_minutes' =>
+"30 minuuttia",
+
+'2_hours' =>
+"2 tuntia",
+
+'12_hours' =>
+"12 tuntia",
+
+'24_hours' =>
+"24 tuntia",
+
+'stats_track_analytics' =>
+"Jäljitä ja laske analyysin datatieto",
+
+'stats_track_analytics_subtext' =>
+"Jos asetuksena on 'ei', kaikki IP-osoitteet ja referenssitiedot jätetään huomioimatta ja tallentamatta. Tämä
+vähentää tallentun tiedon kokonaismäärää, mutta se myös estää tulevia moduulin mahdollisia uusia toimintoja hyödyntämästä näitä tietoja.",
+
+//----------------------------------------
+//	CP - Statistics/Homepage
+//----------------------------------------
+
+'tracker_loading_ellipsis' =>
+"Lataa...",
+
+'tracker_make_settings_default' =>
+"Aseta nämä oletusnäkymäksi?",
+
+'tracker_default_view' =>
+"Oletusnäkymä",
+
+'tracker_everything' =>
+"Kaikki",
+
+'tracker_channel' =>
+"Kanava",
+
+'tracker_channels' =>
+"Kanavat",
+
+'tracker_other_channels' =>
+"Toiset Kanavat",
+
+'tracker_view_options' =>
+"Katso Vaihtoehdoja",
+
+'tracker_view_statistics_for' =>
+"Katso statistiikat näistä",
+
+'tracker_save' =>
+"Tallenna",
+
+'tracker_view_statistics_for_period' =>
+'Katso statistiikka aikavälillä',
+
+'tracker_today' =>
+"Tänään",
+
+'tracker_last_365_days' =>
+"Viimeiset 365 päivää",
+
+'tracker_last_7_days' =>
+"Viimeiset 7 päivää",
+
+'tracker_last_14_days' =>
+"Viimeiset 14 päivää",
+
+'tracker_last_30_days' =>
+"Viimeiset 30 päivää",
+
+'tracker_last_1_days' =>
+"Viimeisin päivä",
+
+'tracker_last_#_days' =>
+"Viimeiset %#% päivää",
+
+'tracker_last_4_weeks' =>
+"Viimeiset 4 viikkoa",
+
+'tracker_last_8_weeks' =>
+"Viimeiset 8 viikkoa",
+
+'tracker_last_12_weeks' =>
+"Viimeiset 12 viikkoa",
+
+'tracker_last_24_weeks' =>
+"Viimeiset 24 viikkoa",
+
+'tracker_last_1_weeks' =>
+"Viime viikko",
+
+'tracker_last_#_weeks' =>
+"Viimeiset %#% viikkoa",
+
+'tracker_last_1_months' =>
+"Viimeinen kuukausi",
+
+'tracker_last_3_months' =>
+"Viimeiset 3 kuukautta",
+
+'tracker_last_6_months' =>
+"Viimeiset 6 kuukautta",
+
+'tracker_last_12_months' =>
+"Viime vuodelta",
+
+'tracker_last_#_months' =>
+"Viimeiset %#% kuukautta",
+
+'tracker_last_year' =>
+"Viime vuosi",
+
+'tracker_all_time' =>
+"Koko historia",
+
+'tracker_this_week' =>
+"Tämä viikko",
+
+'tracker_this_month' =>
+"Tämä kuukausi",
+
+'tracker_this_year' =>
+"Tämä vuosi",
+
+'tracker_alltime' =>
+"Koko historia",
+
+'tracker_days' =>
+"Päivää",
+
+'tracker_weeks' =>
+"Viikkoa",
+
+'tracker_months' =>
+"Kuukautta",
+
+'tracker_hits' =>
+"Osumat",
+
+'tracker_overall_hits_for'=>
+"Kaikk osumat seuraavalle",
+
+'tracker_for'=>
+"seuraavalle",
+
+'tracker_view_by' =>
+"Järjestä",
+
+'tracker_view_statistics_by' =>
+"Järjestä statistikka seuraavanlaisesti",
+
+'tracker_overall_statistics' =>
+"Kaikki statistiikat",
+
+'tracker_channel_statistics' =>
+"Kanavan statistiikat",
+
+'tracker_7_days' =>
+"7 päivää",
+
+'tracker_30_days' =>
+"30 päivää",
+
+'tracker_sitewide' =>
+"Sivustonlaajuisesti",
+
+'tracker_site_averages' =>
+"Sivuston keskiarvot",
+
+'tracker_daily' =>
+"Päivittäinen",
+
+'tracker_weekly' =>
+"Viikottainen",
+
+'tracker_monthly' =>
+"Kuukausittainen",
+
+'tracker_average_hits' =>
+"Osumia keskimäärin",
+
+'tracker_overall' =>
+"Kokonaishistoria",
+
+'tracker_date_range' =>
+"Aikaväli",
+
+'tracker_last' =>
+"Viimeinen",
+
+'tracker_average_hits_per_channel' =>
+"Osumien keskiarvo per kanava",
+
+'tracker_average_hits_per_entry' =>
+"Osumien keskiarvo per julkaisu",
+
+'tracker_average_traffic_share_per_entry' =>
+"Kävijäliikenteen jaon keskiarvo per julkaisu",
+
+'tracker_average_traffic_share' =>
+"Keskimääräinen kävijäliikenteen jako",
+
+'tracker_entry_statistics' =>
+"Julkaisun statistiikka",
+
+'tracker_entry_title' =>
+"Julkaisun Otsikko",
+
+'tracker_no_results' =>
+"Ei tuloksia!",
+
+'tracker_no_results_chart_ajax' =>
+"Ei voida ladata kaaviota tai tietoa ei ole riittävästi.",
+
+// --------------------------------------------
+//  Publish Page
+// --------------------------------------------
+
+'tracker_new_entry_no_statistics' =>
+"Uusi julkaisu, Ei Statistiikkaa",
+
+'tracker_entry_not_found' =>
+"Julkaisua ei löytynyt",
+
+'tracker_entry_no_statistics' =>
+"Tälle julkaisulle ei löytynyt statistiikkaa.",
+
+// --------------------------------------------
+//  Trim Tracker Data
+// --------------------------------------------
+
+'tracker_trim_data' =>
+"Vakauta Osumalaskurin raakatieto",
+
+'tracker_trim_data_subtext' =>
+"Tämä mahdollistaa vanhan tiedon poistamisen moduulista, joka pienentää tietokantasi kokoa sekä parantaa käyttönopeutta.
+Suositellaan sivustoille, joissa on useita tuhansia julkaisuja joista Osumalaskuri kerää katselukertojen tietoja.",
+
+'tracker_trim_data_warning' =>
+"Poistamine ja tietokannan päivitys voi kestää hetken. Älä sulje selaintasi ennenkuin tämä prosessi on suoritettu kokonaan.",
+
+'tracker_limit_tracker_data_to' =>
+"Rajaa statistiikkatiedot",
+
+'tracker_last_two_years' =>
+"Viimeiset 2 vuotta",
+
+'tracker_stats_cleaned_out' =>
+"Statistiikat ovat onnistuneesti poistettu.",
+
+//----------------------------------------
+//	General
+//----------------------------------------
+
+"submit" =>
+"Hyväksy",
+
+"search" =>
+"Hae",
+
+"edit" =>
+"Muokkaa",
+
+"action_can_not_be_undone" =>
+"Tätä toimintoa EI VOIDA peruuttaa.",
+
+// -------------------------------------
+//	Stat Counts
+// -------------------------------------
+
+'update_tracker_stats' =>
+"Päivitä statistiikat",
+
+'tracker_update_stat_count_purpose' =>
+"Tämä päivittää statistiikat jokaiselle julkaisulle käyttämällä AJAXia sekä ilmoittaa Sinulle kun päivitys on suoritettu.
+Klikkaa 'Päivitä kaikki statistiikat' aloittaaksesi.",
+
+'tracker_update_all_entry_stats' =>
+"Päivitä kaikki julkaisujen statistiikat",
+
+"updating_counts_for_entry_" =>
+"Päivitä katselukerrat julkaisulle: ",
+
+'tracker_percent_completed' =>
+"Prosenttia Suoritettu",
+
+'tracker_number_of_entries_updated' =>
+"Päivitettyjen julkaisujen lukumäärä",
+
+'tracker_pause' =>
+"Pysäytä",
+
+'tracker_resume' =>
+"Jatka",
+
+'return_to_tracker_homepage' =>
+"Voit nyt palata moduulin kotisivulle.",
+
+'tracker_stats_recounts_complete' =>
+"Kaikki statistiikat ovat ajantasalla.",
+
+'tracker_stats_updated' =>
+"Statistiikat päivitetty",
+
+'tracker_invalid_values' =>
+"Viallisia arvoja huomattu",
+
+// --------------------------------------------
+//  Tracker 1.x Importer
+// --------------------------------------------
+
+'tracker_entry_imported' =>
+"Osumalaskurin sisältö tuotu",
+
+'tracker_1x_importer' =>
+"Osumalaskuri tiedot versiosta 1.x tuotu",
+
+'tracker_import_subtext' =>
+"Tuo osumat Osumalaskurin versiosta 1.x sekä päivittää statistiikat ajantasalle.",
+
+'tracker_entries_imported' =>
+"Kaikki Osumalaskurin tiedot tuotu onnistuneesti.",
+
+'tracker_import_count_purpose' =>
+"Tämä tuo Osumalaskuri version 1.x tiedot jokaiselle julkaisulle käyttäen AJAXia sekä ilmoittaa sinulle kun tiedot ovat onnistuneesti tuotu.
+Klikkaa 'Tuo Kaikki' aloittaaksesi.",
+
+'tracker_import_all_entries' =>
+"Tuo kaikki julkaisujen tiedot",
+
+'tracker_no_entries_to_import' =>
+"Yhtään julkaisutietoa ei ole saatavilla",
+
+'tracker_1x_entry_imported' =>
+"Osumalaskurin 1.x tiedot tuotu",
+
+// --------------------------------------------
+//  DevDemon Hits Importer - Uses a few of the language variables from Tracker 1.x Importer too
+// --------------------------------------------
+
+'devdemon_hits_entry_imported' =>
+"DevDemon Osumat julkaisusta tuotu",
+
+'devdemon_hits_importer' =>
+"DevDemon Osumat moduulin tuontityökalu",
+
+'hits_importer_subtext' =>
+"Tuo tiedot DevDemon Osumat moduulista",
+
+'devdemon_hits_entries_imported' =>
+"Kaikki tieto liittyen DevDemon moduulin on onnistuneesti tuotu.",
+
+'devdemon_hits_import_count_purpose' =>
+"Tämä tuo tiedot DevDemon Osumalaskurin jokaisesta tallennetusta julkaisusta käyttämällä AJAXia sekä ilmoittaa sinulle koska siirto on onnistuneesti tehty.
+Klikkaa 'Tuo Kaikki Julkaisutiedot' aloittaaksesi.",
+
+'devdemon_hits_entry_imported' =>
+"DevDemon moduulin tiedot onnistuneesti tuotu",
+
+// --------------------------------------------
+//  Errors
+// --------------------------------------------
+
+'tracker_module_disabled' =>
+"Moduuli ei ole käytössä.  Ole hyvä ja tarkista että se on onnistuneesti asennettu menemällä moduulien hallintasivulle käyttämällä 
+ExpressionEnginen hallintapaneelia",
+
+'disable_module_to_disable_extension' =>
+"Jotta voit poistaa tämä lisäosan käytöstä, sinun tulee poistaa käytöstä tätä vastaava <a href='%url%'>moduuli</a>.",
+
+'enable_module_to_enable_extension' =>
+"Jotta voit ottaa tämän lisäosan käyttöön, sinun tulee asentaa tätä vastaava <a href='%url%'>moduuli</a>.",
+
+'invalid_page_request' =>
+"Viallinen pyyntö - Sinulla ei ole oikeuksia tarkastella tätä sivua.",
+
+// --------------------------------------------
+//  Update Routine
+// --------------------------------------------
+
+'update_tracker_module' =>
+"Päivitä Osumalaskurin moduuli",
+
+'tracker_update_message' =>
+"Olet hiljattain ladannut uudemman version Osumalaskurista. Ole hyvä ja suorita päivitys.",
+
+'update_successful' =>
+"Päivitetty Onnistui!",
+
+"tracker_update_successful" =>
+"Moduuli päivitettiin onnistuneesti.",
+
+"tracker_update_failure" =>
+"Jokin meni pieleen yrittäesä päivittää moduulia sen viimeisimpään versioon.",
+
+
+// END
+''=>''
+);
